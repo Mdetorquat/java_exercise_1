c7a1937318a44e21bab71a3674063ff2bdfc968d
Maxence de Torquat <maxence.de-torquat-de-la-coulerie@epita.fr>
2022-02-10 11:42:08 +0100

    push

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static void fibonacci(int n) {
+        int fibo1 = 0;
+        int fibo2 = 1;
+        int fibo3 = 1;
+        for (int i = 2; i <= n; i++) {
+            fibo3 = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibo3;
+        }
+        System.out.print(fibo3);
+    }
     public static void main(String[] args) {
+        System.out.println("Bienvenue");
         String var1 = "quit";
         Scanner clavier = new Scanner(System.in);
-        System.out.println("Bienvenue");
         String var2 = clavier.nextLine();
-        while (!var2.equals(var1)) {
+        String var3 = "fibo";
+        while (!var1.equals(var2)) {
+            if (var3.equals(var2)) {
+                int n = clavier.nextInt();
+                fibonacci(n);
+                break;
+            }
             System.out.println("Unknown command");
             var2 = clavier.nextLine();
         }
-
         clavier.close();
     }
 }
